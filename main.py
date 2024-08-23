3f9f624d884be082ad81e14b8956ff98e2ec764b
konstantinerlich <konstantin.erlich@icloud.com>
2024-08-23 12:03:25 +0200

    put everything inside a class and added inputs for choosing width, steps and rule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-import ast
-import inspect
 import sys
 
 def running_in_terminal():
@@ -16,124 +14,104 @@ if running_in_terminal():
 
 
 
-def scan_functions_in_current_file():
-    # Get the current file's source code
-    current_file = inspect.getsource(inspect.currentframe().f_back)
+class automata():
+    def __init__(self):
+        self.width = 0
+        self.steps = 0
+        self.starting_state = 1
+        self.title = ""
+        self.parameters()
+        self.print_method_names()
+        self.create()
+        self.plot_grid()
 
-    # Parse the source code into an AST
-    tree = ast.parse(current_file)
+    def parameters(self):
+        while True:
+            try:
+                self.width = int(input("Width: "))
+                if self.width <= 0 or self.width % 2 == 0:
+                    print("Width is not a positive odd number.")
+                else:
+                    break
+            except ValueError:
+                print("Please enter a valid integer for width.")
 
-    # List to store function names that start with "rule"
-    rule_functions = []
+        while True:
+            try:
+                self.steps = int(input("Steps: "))
+                if self.steps <= 0:
+                    print("Steps is not a positive integer.")
+                else:
+                    break
+            except ValueError:
+                print("Please enter a valid integer for steps.")
 
-    # Iterate through the nodes of the AST
-    for node in ast.walk(tree):
-        # Check if the node is a function definition
-        if isinstance(node, ast.FunctionDef):
-            # Add the function name to the list if it starts with "rule"
-            if node.name.startswith("rule"):
-                rule_functions.append(node.name)
+        while True:
+            try:
+                self.starting_state = int(input("where to start? 0: left corner, 1: middle, 2: right corner"))
+                if self.starting_state not in [0, 1, 2]:
+                    print("Starting_state is not a valid integer for starting_state.")
+                else:
+                    break
+            except ValueError:
+                print("Please enter a valid integer for starting_state.")
 
-    return rule_functions
+    def grid(self):
+        self.grid = np.zeros((self.steps, self.width), dtype=int)
+        self.midpoint = self.width // 2
+        if self.starting_state == 0:
+            self.grid[0, 0] = 1
+        if self.starting_state == 1:
+            self.grid[0, self.midpoint] = 1  # Start with the middle cell black
+        if self.starting_state == 2:
+            self.grid[0, self.width-1] = 1
+    def condition(self, i, j, a, b, c):
+        # the indexes in grid[i,j] mean the following:
+        # i is the time step, goes down in the plot.
+        # j is the position of the cell, j+1 is right neighbor, j-1 is left neighbor
+        return self.grid[i - 1, j - 1] == a and self.grid[i - 1, j] == b and self.grid[i - 1, j + 1] == c
 
-title = ""
-# Parameters
-width = 201  # Width of the grid (must be odd for a clear center)
-steps = 200  # Number of steps
+    def rule_1(self):
+        global title
+        title = "rule 1"
+        for i in range(1, self.steps):
+            for j in range(1, self.width - 1):
+                if self.condition(i, j, 0, 0, 0):
+                    self.grid[i, j] = 1
 
-# Initialize the grid
-grid = np.zeros((steps, width), dtype=int)
-midpoint = width // 2
-grid[0, midpoint] = 1  # Start with the middle cell black
-
-
-# the indexes in grid[i,j] mean the following:
-# i is the time step, goes down in the plot.
-# j is the position of the cell, j+1 is right neighbor, j-1 is left neighbor
-
-def condition(i, j, a, b, c):
-    return grid[i - 1, j - 1] == a and grid[i - 1, j] == b and grid[i - 1, j + 1] == c
-
-# rules:
-def rule_1():
-    global title
-    title = "rule 1"
-    for i in range(1, steps):
-        for j in range(1, width - 1):
-            if condition(i, j, 0, 0, 0):
-                grid[i, j] = 1
-
-
-def rule_2():
-    global title
-    title = "rule 2"
-    for i in range(1, steps):
-        for j in range(1, width - 1):
-            if grid[i - 1, j + 1] != 0:
-                grid[i, j] = 1
-
-
-def rule_3():
-    global title
-    title = "rule 3"
-    for i in range(1, steps):
-        for j in range(1, width - 1):
-            if grid[i - 1, j - 1] == 0 and grid[i - 1, j] == 0:
-                grid[i, j] = 1
-
-
-def rule_22():
-    global title
-    title = "rule 22"
-    for i in range(1, steps):
-        for j in range(1, width - 1):
-            if (condition(i, j, 0, 1, 0) or
-                    condition(i, j, 0, 0, 1) or
-                    condition(i, j, 1, 0, 0)):
-                grid[i, j] = 1
+    def plot_grid(self):
+        plt.figure(figsize=(20, 20))
+        plt.imshow(self.grid, cmap='binary', interpolation='nearest')
+        plt.title(f"1D Cellular Automaton - {title}")
+        plt.show()
 
 
-def rule_107():
-    global title
-    title = "rule 107"
-    for i in range(1, steps):
-        for j in range(1, width - 1):
-            if (condition(i, j, 1, 1, 0) or
-                    condition(i, j, 1, 0, 1) or
-                    condition(i, j, 0, 1, 1) or
-                    condition(i, j, 0, 0,1) or
-                    condition(i, j, 0, 0, 0)):
-                grid[i, j] = 1
+    def create(self):
+        self.grid()
+        while True:
+            self.code = int(input("type the number of the rule you want to plot: "))
+            match self.code:
+                case 1:
+                    self.rule_1()
+                    break
+                case _:
+                    print("invalid value, rule not found")
+
+    def print_method_names(self):
+        method_names = []
+        # Iterate over the attributes of the class
+        for attr_name in dir(self):
+            # Get the attribute value
+            attr_value = getattr(self, attr_name)
+            # Check if the attribute is callable and starts with 'rule'
+            if callable(attr_value) and not attr_name.startswith('__') and attr_name.startswith('rule'):
+                # Remove 'rule' prefix and add to the list
+                method_names.append(attr_name[len('rule_'):])
+
+        # Print the method names separated by commas
+        print("available methods:" + ", ".join(method_names))
 
 
-#functionality to type rule number to plot it
-print("available rules: ")
-print("Found rules: " + ", ".join(scan_functions_in_current_file()).replace("rule_", ""))
-
-while True:
-    code = int(input("type the number of the rule you want to plot: "))
-    match code:
-        case 1:
-            rule_1()
-            break
-        case 2:
-            rule_2()
-            break
-        case 3:
-            rule_3()
-            break
-        case 22:
-            rule_22()
-            break
-        case 107:
-            rule_107()
-            break
-        case _:
-            print("invalid value, rule not found")
 
 
-# Plotting the grid
-plt.figure(figsize=(20, 20))
-plt.imshow(grid, cmap='binary', interpolation='nearest')
-plt.title(f"1D Cellular Automaton - {title}")
-plt.show()
+ca = automata()
