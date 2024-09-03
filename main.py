4219c970063312fcdbacfa4a5c68149f8e0b4950
konstantinerlich <konstantin.erlich@icloud.com>
2024-09-03 13:23:07 +0200

    cleaned up code

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
-import sys
 import random
-import numpy as np
+import sys
+
 import matplotlib
 import matplotlib.pyplot as plt
+import numpy as np
+
 
 def running_in_terminal():
     """Determine if the script is running in a terminal or an IDE."""
     # Check if sys.stdin is a terminal
     return sys.stdin.isatty() and sys.stdout.isatty()
 
+
 # Use TkAgg backend only if running in a terminal
 if running_in_terminal():
     matplotlib.use('TkAgg')
 
 
-
-class automata():
+class Automata:
     def __init__(self):
         self.width = 0
         self.steps = 0
         self.starting_state = 1
         self.starting_label = ""
         self.probability = 0
-        self.title = ""
+        self.state_to_label = {1: "L", 2: "M", 3: "R"}
 
         self.rule_map = self.build_rule_map()
         self.parameters()
-        self.interface()
-        self.plot_grid(save_path=f"{self.title}-{self.width}x{self.steps}-{self.starting_label}-P{self.probability}.png")
+        self.rule_selection()
+        self.plot_grid(
+            save_path=f"rule-{self.code}-{self.width}x{self.steps}-{self.starting_label}-P{self.probability}.png")
 
     def parameters(self):
         while True:
@@ -52,18 +55,12 @@ class automata():
 
         while True:
             try:
-                self.starting_state = int(input("where to start? 1: left corner, 2: middle, 3: right corner"))
-                if self.starting_state not in [1, 2, 3]:
-                    print("invalid input, please try again.")
-                else:
-                    if self.starting_state == 1:
-                        self.starting_label = "L"
-                    elif self.starting_state == 2:
-                        self.starting_label = "M"
-                    elif self.starting_state == 3:
-                        self.starting_label = "R"
-
+                self.starting_state = int(input("Where to start? 1: left corner, 2: middle, 3: right corner "))
+                if self.starting_state in self.state_to_label:
+                    self.starting_label = self.state_to_label[self.starting_state]
                     break
+                else:
+                    print("Invalid input, please try again.")
             except ValueError:
                 print("Please enter a valid integer for the start position.")
 
@@ -79,14 +76,12 @@ class automata():
 
     def make_grid(self):
         self.grid = np.zeros((self.steps, self.width), dtype=int)
-        self.midpoint = self.width // 2
-        if self.starting_state == 1:
-            self.grid[0, 0] = 1
-        if self.starting_state == 2:
-            self.grid[0, self.midpoint] = 1
-        if self.starting_state == 3:
-            self.grid[0, self.width-1] = 1
-
+        starting_positions = {
+            1: 0,  # Left corner
+            2: self.width // 2,  # Middle
+            3: self.width - 1  # Right corner
+        }
+        self.grid[0, starting_positions[self.starting_state]] = 1
 
     def condition(self, i, j, a, b, c):
         # the indexes in grid[i,j] mean the following:
@@ -94,16 +89,10 @@ class automata():
         # j is the position of the cell, j+1 is right neighbor, j-1 is left neighbor
         return self.grid[i - 1, j - 1] == a and self.grid[i - 1, j] == b and self.grid[i - 1, j + 1] == c
 
-    def set_cell(self, i,j):
-        if random.random() < self.probability:
-            self.grid[i, j] = 1
-        else:
-            self.grid[i, j] = 0
-
     def plot_grid(self, save_path=None):
         plt.figure(figsize=(40, 40))
         plt.imshow(self.grid, cmap='binary', interpolation='nearest')
-        plt.title(f"1D Cellular Automaton - {self.title} - Probablity: {self.probability}", fontsize=50)
+        plt.title(f"1D Cellular Automaton - rule {self.code} - Probablity: {self.probability}", fontsize=50)
         plt.xticks([])
         plt.yticks(fontsize=45)
         plt.ylabel("Steps", fontsize=50)
@@ -112,7 +101,6 @@ class automata():
             plt.savefig(save_path, bbox_inches='tight')
 
         plt.show()
-
 
     def build_rule_map(self):
         rule_map = {}
@@ -128,7 +116,7 @@ class automata():
         rule_numbers = sorted(self.rule_map.keys())
         print("Available rules: " + ", ".join(map(str, rule_numbers)))
 
-    def interface(self):
+    def rule_selection(self):
         self.make_grid()
         self.print_available_rules()
         while True:
@@ -142,61 +130,57 @@ class automata():
             except ValueError:
                 print("Please enter a valid integer")
 
-
-
     def loop_over_grid(self, rule):
         for i in range(1, self.steps):
             for j in range(1, self.width - 1):
                 if rule(i, j):
-                    self.set_cell(i, j)
-    #RULES:
-    def rule_1(self, i,j):
-        self.title = "rule 1"
+                    self.grid[i, j] = int(random.random() < self.probability)
+
+    # RULES:
+    def rule_1(self, i, j):
         return self.condition(i, j, 0, 0, 0)
-    def rule_2(self,i, j):
-        self.title = "rule 2"
+
+    def rule_2(self, i, j):
         return self.grid[i - 1, j + 1] != 0
-    def rule_3(self,i, j):
-        self.title = "rule 3"
+
+    def rule_3(self, i, j):
         return self.grid[i - 1, j - 1] == 0 and self.grid[i - 1, j] == 0
 
-    def rule_22(self,i, j):
-        self.title = "rule 22"
+    def rule_22(self, i, j):
         return (self.condition(i, j, 0, 1, 0) or
-                        self.condition(i, j, 0, 0, 1) or
-                        self.condition(i, j, 1, 0, 0))
+                self.condition(i, j, 0, 0, 1) or
+                self.condition(i, j, 1, 0, 0))
 
     def rule_45(self, i, j):
-        self.title = "rule 45"
         return (self.condition(i, j, 1, 0, 1) or
                 self.condition(i, j, 0, 1, 1) or
                 self.condition(i, j, 0, 1, 0) or
                 self.condition(i, j, 0, 0, 0))
-    def rule_86(self, i,j):
-        self.title = "rule 86"
+
+    def rule_86(self, i, j):
         return (self.condition(i, j, 1, 1, 0) or
                 self.condition(i, j, 1, 0, 0) or
                 self.condition(i, j, 0, 1, 0) or
                 self.condition(i, j, 0, 0, 1))
 
-    def rule_107(self, i,j):
-        self.title = "rule 107"
+    def rule_107(self, i, j):
         return (self.condition(i, j, 1, 1, 0) or
-                        self.condition(i, j, 1, 0, 1) or
-                        self.condition(i, j, 0, 1, 1) or
-                        self.condition(i, j, 0, 0, 1) or
-                        self.condition(i, j, 0, 0, 0))
-    def rule_105(self,i,j):
-        self.title = "rule 105"
-        return (self.condition(i, j, 1, 1, 0) or
-                        self.condition(i, j, 1, 0, 1) or
-                        self.condition(i, j, 0, 1, 1) or
-                        self.condition(i, j, 0, 0, 0))
-    def rule_225(self, i,j):
-        self.title = "rule 225"
-        return(self.condition(i, j, 1, 1, 1) or
-                        self.condition(i, j, 1, 1, 0) or
-                        self.condition(i, j, 1, 0, 1) or
-                        self.condition(i, j, 0, 0, 0))
+                self.condition(i, j, 1, 0, 1) or
+                self.condition(i, j, 0, 1, 1) or
+                self.condition(i, j, 0, 0, 1) or
+                self.condition(i, j, 0, 0, 0))
 
-ca = automata()
+    def rule_105(self, i, j):
+        return (self.condition(i, j, 1, 1, 0) or
+                self.condition(i, j, 1, 0, 1) or
+                self.condition(i, j, 0, 1, 1) or
+                self.condition(i, j, 0, 0, 0))
+
+    def rule_225(self, i, j):
+        return (self.condition(i, j, 1, 1, 1) or
+                self.condition(i, j, 1, 1, 0) or
+                self.condition(i, j, 1, 0, 1) or
+                self.condition(i, j, 0, 0, 0))
+
+
+ca = Automata()
